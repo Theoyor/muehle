76101eca44f6522035fff4809472e8f8f8bdc1b9
juliusclausnitzer <julius.clausnitzer@gmail.com>
2020-06-02 17:46:29 +0200

    es lübbt

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,12 @@
 mod base;   
 use base::base::State;
+mod screen;
+use screen::start;
 
 fn main() {
     println!("Hello, world!");
     let fd = State::new();
     println!("{}",fd.spot_muehle((1,1,1)));
+
+    screen::start();
 }
