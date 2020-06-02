293bba2a4428176c0410bef9da0d5a7b587c5a52
philippkb <65279062+philippkb@users.noreply.github.com>
2020-06-02 19:38:02 +0200

    heapless vector statt vector auf dem heap + heapless crate

--- src/base.rs
+++ src/base.rs
@@ -1,4 +1,9 @@
+
+
 pub mod base{
+    use heapless::Vec;
+    use heapless::consts::U24;
+
     pub enum PlayMode{
         Place,
         Move,
@@ -9,7 +14,7 @@ pub mod base{
         //* (x,y,p) ist ein field mit x: x-Achse des Feldes, y: y-Achse des Feldes, und bei p = 0 ist Field unbesetzt
         //* p = 1 von Spieler 1 besetzt und bei p = -1 von Spieler 2 (bzw. dem Computer) besetzt
 
-        pub board: Vec<(i8,i8,i8)>,
+        pub board: Vec<(i8,i8,i8), U24>,
         pub p1_mode: PlayMode,
         pub p2_mode: PlayMode,
         pub turn: i8,
@@ -18,20 +23,24 @@ pub mod base{
     impl State{
         pub fn new() -> State{
             //* Gibt einen State mit "leerem" board zurück. Sortiert nach Spalten.
+            let mut xs: Vec<(i8,i8,i8), U24> = Vec::new();
+            xs.extend_from_slice(&[(1, 1, 0), (1, 4, 0), (1, 7, 0), (2, 2, 0), (2, 4, 0), (2, 6, 0), (3, 3, 0), (3, 4, 0), (3, 5, 0), (4, 1, 0), (4, 2, 0), (4, 3, 0), (4, 5, 0), (4, 6, 0), (4, 7, 0), (5, 3, 0), (5, 4, 0), (5, 5, 0), (6, 2, 0), (6, 4, 0), (6, 6, 0), (7, 1, 0), (7, 4, 0), (7, 7, 0)]).unwrap();
+
+
             State{
-                board : vec![(1,1,0),(1,4,0),(1,7,0),(2,2,0),(2,4,0),(2,6,0),(3,3,0),(3,4,0),(3,5,0),(4,1,0),(4,2,0),(4,3,0),(4,5,0),(4,6,0),(4,7,0),(5,3,0),(5,4,0),(5,5,0),(6,2,0),(6,4,0),(6,6,0),(7,1,0),(7,4,0),(7,7,0)],
+                board: xs,
                 p1_mode: PlayMode::Place,
                 p2_mode: PlayMode::Place,
                 turn: 1, //TODO evtl. Seiten auswaehlen
             }
         }
 
-        pub fn sort_by_lines(&self) -> Vec<(i8,i8,i8)>{
-            let mut ret: Vec<(i8,i8,i8)> = Vec::new(); 
+        pub fn sort_by_lines(&self) -> Vec<(i8,i8,i8), U24>{
+            let mut ret: Vec<(i8,i8,i8), U24> = Vec::new();
             for i in 1..8{
                 for field in &self.board {
                     if field.2 == i{
-                        ret.push(*field)
+                        ret.push(*field);
                     } 
                 }
             }
@@ -85,15 +94,19 @@ pub mod base{
         }
 
         pub fn move_control(&self,from:(i8,i8,i8),to:(i8,i8,i8)) -> bool{
+            // wenn das Feld besetzt ist
             if to.2 != 0 {
                 return false
             }
+            // wenn ein falscher Stein bewegt werden soll
             if self.turn != from.2 {
                 return false
             }
+            // wenn das Zielfeld das Ursprungsfeld berührt
             if from.0 == to.0 || from.1 == to.1 {
                 return true
             }
+            // wenn im Jumpmode true sonst false
             if self.turn == 1  {
                 match self.p1_mode {
                     PlayMode::Place => return false,
