b0e0704e20b507a66d87b07f2492214ef46891ff
philippkb <65279062+philippkb@users.noreply.github.com>
2020-06-05 17:47:35 +0200

    gelayouted

--- src/main.rs
+++ src/main.rs
@@ -9,10 +9,6 @@ fn main() {
     fd.printm();
 
     println!("{}",fd.spot_muehle((1,1,1)));
-    let fd = fd.change(4, 1, 1).unwrap();
-    let fd = fd.change(4, 2, 1).unwrap();
-    let fd = fd.change(4, 3, 1).unwrap();
-    let fd = fd.change(4, 7, 1).unwrap();
     fd.printm();
     println!("{}",fd.spot_muehle((4,1,1)));
     screen::start();
