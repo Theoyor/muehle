76ce0af7f4466c27ea4e7f2255112a4768179bca
Theoyor <59316058+Theoyor@users.noreply.github.com>
2020-06-09 16:04:20 +0200

    pub fn place

--- src/main.rs
+++ src/main.rs
@@ -6,10 +6,9 @@ use screen::start;
 fn main() {
     println!("Hello, world!");
     let fd = State::new();
-    fd.printm();
 
     println!("{}",fd.spot_muehle((1,1,1)));
-    fd.printm();
+    print!("{:?}", fd);
     println!("{}",fd.spot_muehle((4,1,1)));
     screen::start();
 }
