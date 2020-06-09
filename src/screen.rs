e09aff2bf746de0d3a9c3366a69f91c5b8cb5aed
juliusclausnitzer <julius.clausnitzer@gmail.com>
2020-06-09 17:55:34 +0200

    lines

--- src/screen.rs
+++ src/screen.rs
@@ -37,7 +37,7 @@ impl event::EventHandler for MainState {
         let rectsize1 = graphics::Rect::new(100.0, 100.0, 400.0, 400.0);
         let rect1 = graphics::Mesh::new_rectangle(
             ctx, 
-            graphics::DrawMode::stroke(3.0), 
+            graphics::DrawMode::stroke(4.0), 
             rectsize1, 
             graphics::BLACK
         )?;
@@ -47,7 +47,7 @@ impl event::EventHandler for MainState {
         let rectsize2 = graphics::Rect::new(175.0, 175.0, 250.0, 250.0);
         let rect2 = graphics::Mesh::new_rectangle(
             ctx, 
-            graphics::DrawMode::stroke(3.0), 
+            graphics::DrawMode::stroke(4.0), 
             rectsize2, 
             graphics::BLACK
         )?;
@@ -58,17 +58,51 @@ impl event::EventHandler for MainState {
         let rectsize2 = graphics::Rect::new(250.0, 250.0, 100.0, 100.0);
         let rect2 = graphics::Mesh::new_rectangle(
             ctx, 
-            graphics::DrawMode::stroke(3.0), 
+            graphics::DrawMode::stroke(4.0), 
             rectsize2, 
             graphics::BLACK
         )?;
         graphics::draw(ctx, &rect2, (na::Point2::new(0.0, 0.0),))?;
         
         //Verbindungslinien:
+
+        let line1size = graphics::Rect::new(100.0, 300.0, 150.0, 0.0);
+        let line1 = graphics::Mesh::new_rectangle(
+            ctx, 
+            graphics::DrawMode::stroke(4.0), 
+            line1size, 
+            graphics::BLACK
+        )?;
+        graphics::draw(ctx, &line1, (na::Point2::new(0.0, 0.0),))?;
+
+        let line2size = graphics::Rect::new(350.0, 300.0, 150.0, 0.0);
+        let line2 = graphics::Mesh::new_rectangle(
+            ctx, 
+            graphics::DrawMode::stroke(4.0), 
+            line2size, 
+            graphics::BLACK
+        )?;
+        graphics::draw(ctx, &line2, (na::Point2::new(0.0, 0.0),))?;
+
+        let line3size = graphics::Rect::new(300.0, 350.0, 0.0, 150.0);
+        let line3 = graphics::Mesh::new_rectangle(
+            ctx, 
+            graphics::DrawMode::stroke(4.0), 
+            line3size, 
+            graphics::BLACK
+        )?;
+        graphics::draw(ctx, &line3, (na::Point2::new(0.0, 0.0),))?;
+
+        let line4size = graphics::Rect::new(300.0, 100.0, 0.0, 150.0);
+        let line4 = graphics::Mesh::new_rectangle(
+            ctx, 
+            graphics::DrawMode::stroke(4.0), 
+            line4size, 
+            graphics::BLACK
+        )?;
+        graphics::draw(ctx, &line4, (na::Point2::new(0.0, 0.0),))?;
         
-        //let line1= graphics::Mesh::new_line(ctx, &[na::Point2::new(10.0, 10.0), na::Point2::new(10.0, 20.0)], 3.0, graphics::BLACK);
-        
-        //graphics::draw(ctx, &line1, (na::Point2::new(0.0,0.0),))?;
+
         
 
         graphics::present(ctx)?;
