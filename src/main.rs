df02761846aee7e21bb6d9bca37a10c218c28bf1
juliusclausnitzer <julius.clausnitzer@gmail.com>
2020-06-30 18:26:40 +0200

    white2

--- src/main.rs
+++ src/main.rs
@@ -254,7 +254,7 @@ impl event::EventHandler for MainState {
                 let spielstein= graphics::Mesh::new_circle(ctx, graphics::DrawMode::stroke(4.0), na::Point2::new(steinkoordinaten.0, steinkoordinaten.1), 18.0, 0.1, graphics::BLACK).unwrap();
                 graphics::draw(ctx, &spielstein, (na::Point2::new(0.0, 0.0),))?;
                 
-                let spielstein= graphics::Mesh::new_circle(ctx, graphics::DrawMode::stroke(4.0), na::Point2::new(steinkoordinaten.0, steinkoordinaten.1), 14.0, 0.1, graphics::WHITE).unwrap();
+                let spielstein= graphics::Mesh::new_circle(ctx, graphics::DrawMode::fill(), na::Point2::new(steinkoordinaten.0, steinkoordinaten.1), 16.0, 0.1, graphics::WHITE).unwrap();
                 graphics::draw(ctx, &spielstein, (na::Point2::new(0.0, 0.0),))?;
             }
             
